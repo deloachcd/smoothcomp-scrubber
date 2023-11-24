9d48ad16c6b3bc893b2447f617517ae068ff83af
Chandler DeLoach <deloach.cdd@gmail.com>
2023-11-23 22:35:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# smoothcomp-scrubber
+Have you ever needed to manually scrub through Smoothcomp streams, to find
+the timestamps where you or your teammates actually show up to fight? If so,
+this script is for you. It allows you to take a video file containing the 
+archive of a Smoothcomp tournament stream and automatically write the
+timestamps where the people you list show up to a CSV file.
+
+## Pre-Requisites
+This depends on Google's `tesseract-ocr` engine, so you need that installed.
+MacOS has it through homebrew, your favorite Linux distro probably has it in
+its package manager, and Windows users should be using WSL and following 
+instructions for Linux distros with stuff like this script anyway.
+
+## Basic setup (once you have `tesseract-ocr` installed)
+```
+git clone https://github.com/deloachcd/smoothcomp-scrubber.git
+cd smoothcomp-scrubber
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+```
+
+## Basic usage
+The basic workflow with this program is:
+- obtain the archive of the smoothcomp stream you want to scrub
+  through, in a format like .MOV or .MP4 etc. (`yt-dlp` is a pretty
+  good tool for this)
+- write a list of competitor names to find in the stream to a CSV file,
+  in `firstname, lastname` format (names should be written as they're
+  going to appear in the smoothcomp stream, case-insensitive)
+- call the script, passing in the video archive and competitor list files
+  as arguments, and either also specifying an output file or just reading 
+  from the default `output.csv` once you want to see where the competitors
+  you listed show up. the script has a `-h` option that will tell you all
+  the flags you need to get going
+
+## TODOs for me to do later
+- Exceptions. This script has none of them right now, and they would help
+  out for sure.
---
+++ get-smoothcomp-timestamps.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+import argparse
+import csv
+import datetime
+import time
+
+import cv2
+import pytesseract
+
+
+def crop_frame_to_competitor_names(frame, height, width):
+    # crop to the section of the stream that's actually relevant
+    # to our OCR engine - the small section where names actually
+    # show up
+    return frame[(height//4)-(height//32):(height//2)+(height//32), 
+                 (width//8)-(width//32):(7*(width//8))+(width//32)]
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str, help="path to input video file [required]")
+ap.add_argument("-f", "--competitors-file", type=str, default="competitors.csv",
+	            help="path to input file listing competitors (default:competitors.csv)")
+ap.add_argument("-o", "--output-file", type=str, default="output.csv",
+	            help="path to input file listing competitors (default:output.csv)")
+ap.add_argument("-s", "--seconds", type=float, default=10,
+                help="seconds between OCR captures to check for competitor names (default:10)")
+args = vars(ap.parse_args())
+
+# competitor_names list will be used to check for relevant names
+# in OCR-captured strings
+competitor_names = []
+with open(args["competitors_file"], "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    for row in reader:
+        firstname, lastname = row[0].strip(), row[1].strip()
+        competitor_names.append([firstname, lastname])
+
+video = cv2.VideoCapture(args["video"])
+video_fps = video.get(cv2.CAP_PROP_FPS)
+video_frames_total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+print("== INITIALIZING ==")
+print(f"Video filename: {args['video']}")
+print(f"Video FPS: {video_fps}")
+print(f"Comptitor list: {args['competitors_file']}")
+print(f"Output filename: {args['output_file']}")
+print(f"Seconds between OCR capture frames: {args['seconds']}")
+FRAMES_TO_ITERATE = int(args["seconds"] * video_fps)
+
+# read shape from first frame so we don't need to get dimensions on
+# each loop iteration
+start_time = time.time()
+rval, first_frame = video.read()
+f_height, f_width, f_channels = first_frame.shape
+video_time = datetime.timedelta(seconds=0)
+output_file = open(args["output_file"],"a")
+
+# start scanning through the video, looking for instances of listed
+# competitor names
+print("\n== SCANNING ==")
+for current_frame in range(0, video_frames_total, FRAMES_TO_ITERATE):
+    video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
+    rval, frame = video.read()
+    if not rval:
+        break
+    
+    # we make the frame from our video that OCR has to process smaller,
+    # by converting it to grayscale and cropping it only to the area
+    # where the relevant competitor names will actually show up on
+    # the stream
+    ocr_frame = crop_frame_to_competitor_names(
+        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), f_height, f_width
+    )
+    frame_as_str = pytesseract.image_to_string(ocr_frame).lower()
+    detected_competitor_names = []
+    for firstname, lastname in competitor_names:
+        if firstname in frame_as_str and lastname in frame_as_str:
+            output_file.write(f"{firstname} {lastname},{video_time}\n")
+            output_file.flush()
+            detected_competitor_names.append(f"found {firstname}")
+    video_time += datetime.timedelta(seconds=args["seconds"])
+    print(f"{video_time} -- {(current_frame/video_frames_total)*100:.2f}%"
+          + " video scanned... " + ", ".join(detected_competitor_names))
+output_file.close()
+
+print("== SUCCESS ==")
+print(f"Scanned through {video_frames_total} frames in {time.time() - start_time}s")
