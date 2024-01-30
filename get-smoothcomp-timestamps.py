cbd456a451e457c9e649da6e4a74f9834aa9f6cb
Chandler DeLoach <deloach.cdd@gmail.com>
2024-01-29 22:24:42 -0700

    name constants properly

--- get-smoothcomp-timestamps.py
+++ get-smoothcomp-timestamps.py
@@ -32,50 +32,50 @@ ap.add_argument("-p", "--psm", type=str, default=11,
                 help="have tesseract-ocr use a specific PSM (default:11)")
 ap.add_argument("-d", "--debug", action="store_true",
                 help="print OCR capture strings as the program runs")
-args = vars(ap.parse_args())
+ARGS = vars(ap.parse_args())
 
 # competitor_names list will be used to check for relevant names
 # in OCR-captured strings
 competitor_names = []
-with open(args["competitors_file"], "r") as infile:
+with open(ARGS["competitors_file"], "r") as infile:
     for row in infile.readlines():
         competitor_names.append(row.replace("\n","").strip())
 
-video = cv2.VideoCapture(args["video"])
-video_fps = video.get(cv2.CAP_PROP_FPS)
-video_frames_total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+video = cv2.VideoCapture(ARGS["video"])
+VIDEO_FPS = video.get(cv2.CAP_PROP_FPS)
+VIDEO_FRAMES_TOTAL = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 print("== INITIALIZING ==")
-print(f"Video filename: {args['video']}")
-print(f"Video FPS: {video_fps}")
-print(f"Comptitor list: {args['competitors_file']}")
-print(f"Output filename: {args['output_file']}")
-print(f"Seconds between OCR capture frames: {args['seconds']}")
-FRAMES_TO_ITERATE = int(args["seconds"] * video_fps)
+print(f"Video filename: {ARGS['video']}")
+print(f"Video FPS: {VIDEO_FPS}")
+print(f"Comptitor list: {ARGS['competitors_file']}")
+print(f"Output filename: {ARGS['output_file']}")
+print(f"Seconds between OCR capture frames: {ARGS['seconds']}")
+FRAMES_TO_ITERATE = int(ARGS["seconds"] * VIDEO_FPS)
 
 # read shape from first frame so we don't need to get dimensions on
 # each loop iteration
-start_time = time.time()
+START_TIME = time.time()
 rval, first_frame = video.read()
-f_height, f_width, f_channels = first_frame.shape
+F_HEIGHT, F_WIDTH, F_CHANNELS = first_frame.shape
 video_time = timedelta(seconds=0)
-if args["jump_to_timestamp"]:
-    timeskip_str = datetime.strptime(args["jump_to_timestamp"],"%H:%M:%S")
-    initial_timeskip = timedelta(
-        hours=timeskip_str.hour,
-        minutes=timeskip_str.minute,
-        seconds=timeskip_str.second
+if ARGS["jump_to_timestamp"]:
+    TIMESKIP_STR = datetime.strptime(ARGS["jump_to_timestamp"],"%H:%M:%S")
+    INITIAL_TIMESKIP = timedelta(
+        hours=TIMESKIP_STR.hour,
+        minutes=TIMESKIP_STR.minute,
+        seconds=TIMESKIP_STR.second
     )
-    video_time += initial_timeskip
-    first_frame = int(initial_timeskip.seconds * video_fps)
+    video_time += INITIAL_TIMESKIP
+    first_frame = int(INITIAL_TIMESKIP.seconds * VIDEO_FPS)
 else:
     first_frame = 0
-output_file = open(args["output_file"],"w")
+output_file = open(ARGS["output_file"],"w")
 
 # start scanning through the video, looking for instances of listed
 # competitor names
 print("\n== SCANNING ==")
-for current_frame in range(first_frame, video_frames_total, FRAMES_TO_ITERATE):
+for current_frame in range(first_frame, VIDEO_FRAMES_TOTAL, FRAMES_TO_ITERATE):
     video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
     rval, frame = video.read()
     if not rval:
@@ -86,13 +86,14 @@ for current_frame in range(first_frame, video_frames_total, FRAMES_TO_ITERATE):
     # where the relevant competitor names will actually show up on
     # the stream
     ocr_frame = crop_frame_to_competitor_names(
-        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), f_height, f_width
+        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), F_HEIGHT, F_WIDTH
     )
     # From the Tesseract docs:
     # PSM=11: Sparse text. Find as much text as possible in no particular order.
     # This seems to be faster and more accurate than the default method about
     # getting the names we're looking for.
-    frame_as_str = pytesseract.image_to_string(ocr_frame,config=f"--psm {args['psm']}")
+    frame_as_str = pytesseract.image_to_string(ocr_frame,
+                                               config=f"--psm {ARGS['psm']}")
     detected_competitor_names = []
     for name in competitor_names:
         lowered_frame_str = frame_as_str.lower()
@@ -103,14 +104,14 @@ for current_frame in range(first_frame, video_frames_total, FRAMES_TO_ITERATE):
             output_file.write(f"{name},{video_time}\n")
             output_file.flush()
             detected_competitor_names.append(f"found {name}")
-    video_time += timedelta(seconds=args["seconds"])
-    if args["debug"]:
+    video_time += timedelta(seconds=ARGS["seconds"])
+    if ARGS["debug"]:
         print("== DEBUG START ==")
         print(frame_as_str)
         print("== DEBUG END ==")
-    print(f"{video_time} -- {(current_frame/video_frames_total)*100:.2f}%"
+    print(f"{video_time} -- {(current_frame/VIDEO_FRAMES_TOTAL)*100:.2f}%"
           + " video scanned... " + ", ".join(detected_competitor_names))
 output_file.close()
 
 print("== SUCCESS ==")
-print(f"Scanned through {video_frames_total} frames in {time.time() - start_time}s")
+print(f"Scanned through {VIDEO_FRAMES_TOTAL} frames in {time.time() - START_TIME}s")
